a3e67bff8dfb9b1de9ba93967150c883d4e02bff
Chris Searle <chris@chrissearle.org>
2025-12-18 21:07:08 +0100

    Add version page

---
+++ app/pages/version.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+const imageTag = computed(() => config.public.imageTag || "development")
+</script>
+
+<template>
+  <div>
+    <h1 class="head1">Version</h1>
+
+    <p>{{ imageTag }}</p>
+  </div>
+</template>
